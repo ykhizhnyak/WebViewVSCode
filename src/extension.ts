2c85caef4debfdac9cf75f259f95d7d3563c1236
Yevgeniy Khizhnyak <ykhizhnyak@whatcom.edu>
2018-10-25 15:04:26 -0700

    d

--- src/extension.ts
+++ src/extension.ts
@@ -76,15 +76,15 @@ class CatCodingPanel {
                 case 'twit':
                     vscode.window.showInformationMessage(message.text);
 
-                    var cb = new Codebird; 
+                    var cb = new Codebird;
                     cb.setConsumerKey('IkGv5KFw7cpnNP96840j3jt0H', 'rdGAXK7eWQfU3mUTlZNLBEhLRGynY1N19bNglPH6ZFHLewgZMp');
                     cb.setToken('491981647-OaQ2FoxqGSDlaMyEI6MUpq2Gg9mOpZ4nTKg2G973', 'EuvhRxKtubT7N1JJCjE1xAipFjR1C02MH7rq4bHFpCCrX');
 
                     var params = {
                         status: 'Twit from extension' //document.getElementById('twitText').value
                     };
-                    cb.__call("statuses_update", params, function(reply, rate, err) {
-                        vscode.showErrorMessage(err);
+                    cb.__call("statuses_update", params, function(reply) {
+                        vscode.showErrorMessage(reply);
                     });
 
                     return;
